8b13757c5ce910145c2491660795e59a0ad1d236
Chris Thomson <chris@cthomson.ca>
2016-09-13 10:54:41 -0400

    Add optional time_source param to pass to CronParser
    This allows people to send in Time.zone so the cron tasks can be
    scheduled in a particular timezone.

--- lib/em/cron.rb
+++ lib/em/cron.rb
@@ -5,6 +5,7 @@ require "em/cron/version"
 module EM
   class Cron
     # @param cron_string [String]
+    # @param time_source [Class]
     # @yield [Time] the time the block was scheduled to be called by cron. If
     #   yielding this block returns `:stop` then the schedule stops.
     # @example
@@ -13,10 +14,10 @@ module EM
     #       puts "hello world at time: #{time}"
     #     end
     #   end
-    def self.schedule(cron_string, &blk)
-      cron_parser = CronParser.new(cron_string)
-      next_time = cron_parser.next(Time.now)
-      EM.add_timer(next_time - Time.now) do
+    def self.schedule(cron_string, time_source = Time, &blk)
+      cron_parser = CronParser.new(cron_string, time_source)
+      next_time = cron_parser.next(time_source.now)
+      EM.add_timer(next_time - time_source.now) do
         result = yield(next_time)
         schedule(cron_string, &blk) unless result == :stop
       end
